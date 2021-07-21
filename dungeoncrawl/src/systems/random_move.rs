74cb451064fda8c5d3c452ebde39dda0b94d7d7f
jfaust <jordan.faust@disneystreaming.com>
2021-07-21 17:26:26 -0500

    P2. CH7. Add MovingRandomly component and systems
    * Add component that marks entities as moving randomly
    * Add system to randomly move an entity

---
+++ dungeoncrawl/src/systems/random_move.rs
@@ -0,0 +1,27 @@
+use crate::prelude::*;
+
+#[system]
+#[write_component(Point)]
+#[read_component(MovingRandomly)]
+pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
+    // Get the point and moving randomly component of all entities that are
+    // moving randomly.
+    let mut movers = <(&mut Point, &MovingRandomly)>::query();
+
+    // Randomly move each mover in a random direction
+    movers
+        .iter_mut(ecs)
+        .for_each(|(position, _)| {
+            let mut rng = RandomNumberGenerator::new();
+            let destination = match rng.range(0, 4) {
+                0 => Point::new(-1, 0),
+                1 => Point::new(1, 0),
+                2 => Point::new(0, -1),
+                _ => Point::new(0, 1),
+            } + *position;
+
+            if map.can_enter_tile(destination) {
+                *position = destination
+            }
+        })
+}
